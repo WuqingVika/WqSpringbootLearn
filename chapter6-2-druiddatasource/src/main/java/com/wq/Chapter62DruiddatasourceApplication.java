49f015e14fc2ca4a65aa1b5dfff8667e31410464
wuqingvika <526288022@qq.com>
2018-03-29 23:26:29 +0800

    20180329druid

---
+++ chapter6-2-druiddatasource/src/main/java/com/wq/Chapter62DruiddatasourceApplication.java
@@ -0,0 +1,12 @@
+package com.wq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter62DruiddatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter62DruiddatasourceApplication.class, args);
+	}
+}
---
+++ chapter6-2-druiddatasource/src/main/java/com/wq/config/DruidConfig.java
@@ -0,0 +1,24 @@
+package com.wq.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by wuqingvika on 2018/3/29.
+ */
+@Configuration
+public class DruidConfig {
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druid(){
+        return new DruidDataSource();
+    }
+
+    //配置Druid的监控
+    //配置一个管理后台的Servlet
+
+}
