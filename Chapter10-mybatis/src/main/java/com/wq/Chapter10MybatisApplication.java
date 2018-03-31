bf50c08987a57733ccc0e3b5d2ae7b245d26b0df
wuqingvika <526288022@qq.com>
2018-03-31 22:39:55 +0800

    配置sql加载自动建表

---
+++ Chapter10-mybatis/src/main/java/com/wq/Chapter10MybatisApplication.java
@@ -0,0 +1,12 @@
+package com.wq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter10MybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter10MybatisApplication.class, args);
+	}
+}
